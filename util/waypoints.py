54cfc6cd3b997b6a6f92d061e9f1aef5b8ac08dc
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-18 10:24:45 +0200

    Update waypoints.py

--- util/waypoints.py
+++ util/waypoints.py
@@ -201,6 +201,12 @@ class waypoint():
                     map_url = f"{self.config.map_url}@gym/{self.id}"
             elif self.config.map_provider == "rmad":
                 map_url = f"{self.config.map_url}?lat={self.lat}&lon={self.lon}&zoom=18"
+            elif self.config.map_provider == "reactmap":
+                map_url = f"{self.config.map_url}/id"
+                if self.type == "stop":
+                    map_url = f"/pokestops/{self.id}"
+                elif self.type == "gym":
+                    map_url = f"/gyms/{self.id}"
             links = f"{links} \\| [{self.config.map_name}]({map_url})"
         
         address = ""
